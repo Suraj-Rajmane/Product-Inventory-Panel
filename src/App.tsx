a740da883a0053997a1945f895df28eab20a1b80
Suraj-Rajmane <surajrajmane18@gmail.com>
2025-12-24 19:44:34 +0530

    Setup React Query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import TestQuery from "./TestQuery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Route path="/products" element={<div>Products Page</div>} />
         <Route path="/products/new" element={<div>Add Product Page</div>} />
       </Routes>
+      <TestQuery></TestQuery>
     </>
   );
 }
